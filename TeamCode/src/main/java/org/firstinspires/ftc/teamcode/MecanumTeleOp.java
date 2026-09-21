b29ea758637c62d1df9d20acb97d847e74817c34
Denial <bandote63@gmail.com>
2026-09-21 16:32:45 -0700

    bonb

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Blinker;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+@TeleOp(name = "Fahhhhhhh")
+public class MecanumTeleOp {
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -1,10 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 public class Teleop {
-    // Cn
-    public static void main(String[] args){
-        System.out.println("Cancer");
-    }
 
 }
 
