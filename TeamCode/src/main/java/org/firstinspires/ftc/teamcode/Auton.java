93fe5ccd64f846748cbf1fa3da6b78f36352e8a4
Denial <bandote63@gmail.com>
2026-09-23 15:48:11 -0700

    bonb

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import java.util.*;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Blinker;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="Auton")
+public class Auton {
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor frontLeftDrive=null;
+    private DcMotor backLeftDrive=null;
+    private DcMotor frontRightDrive=null;
+    private DcMotor backRightDrive=null;
+
+    public void runOpMode() throws InterruptedException{
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "front_left_drive");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "back_left_drive");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "front_right_drive");
+        backRightDrive = hardwareMap.get(DcMotor.class, "back_right_drive");
+
+        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
+        backRightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        waitForStart();
+        runtime.reset();
+
+    }
+}
